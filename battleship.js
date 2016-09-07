745dee5cf0d331d9532e4c9f986d5a40f1fad02b
kyle lao <kylelao3201@gmail.com>
2016-09-07 09:18:32 -0700

    tried hint 3

--- battleship.js
+++ battleship.js
@@ -82,8 +82,8 @@ var gameBoard = [
 function fireTorpedo() {
 
 	// Your game logic will go here!
-var userInput = $("#userGuess").val();
-X.substring(0,4)
+var userGuess = $("#userInput").val();
+
 var row = userInput.substring(0,1);
-var column userInput.substring(1,2);
+var cols = userInput.substring(1,2);
 }
